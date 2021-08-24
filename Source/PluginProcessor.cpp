c3c8ffde9ff7b2ea22f4a06220631356c2b13459
Gerardo Tovar Cervantes <Gerardo@MacBook-Pro-de-Gerardo-2.local>
2021-08-24 14:54:51 -0500

    Added Parameters

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -166,7 +166,8 @@ bool EelEQAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* EelEQAudioProcessor::createEditor()
 {
-    return new EelEQAudioProcessorEditor (*this);
+    //return new EelEQAudioProcessorEditor (*this);
+    return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
@@ -182,6 +183,87 @@ void EelEQAudioProcessor::setStateInformation (const void* data, int sizeInBytes
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
 }
+
+
+//==============================================================================
+juce::AudioProcessorValueTreeState::ParameterLayout EelEQAudioProcessor::createParameterLayout() {
+    
+    juce::AudioProcessorValueTreeState::ParameterLayout layout;
+    
+    //Float Parameters
+    
+    layout.add(
+               std::make_unique<juce::AudioParameterFloat>("LowCut Freq",
+                                                           "LowCut Freq",
+                                                           juce::NormalisableRange<float>(20.f,20000.f,1.f,1.f),
+                                                           20.f
+                                                           )
+               );
+    
+    layout.add(
+               std::make_unique<juce::AudioParameterFloat>("HighCut Freq",
+                                                           "HighCut Freq",
+                                                           juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
+                                                           20000.f
+                                                           )
+               );
+    
+    layout.add(
+               std::make_unique<juce::AudioParameterFloat>("Peak Freq",
+                                                           "Peak Freq",
+                                                           juce::NormalisableRange<float>(20.f,20000.f,1.f,1.f),
+                                                           1000.f
+                                                           )
+               );
+    
+    layout.add(
+               std::make_unique<juce::AudioParameterFloat>("Peak Gain",
+                                                           "Peak Gain",
+                                                           juce::NormalisableRange<float>(-24.f,24.f,0.1f,1.f),
+                                                           0.f
+                                                           )
+               );
+    
+    layout.add(
+               std::make_unique<juce::AudioParameterFloat>("Quality",
+                                                           "Quality",
+                                                           juce::NormalisableRange<float>(0.1f,15.f,0.05f,1.f),
+                                                           0.9f
+                                                           )
+               );
+    
+    //Choice Parameters
+    
+    juce::StringArray stringArray;
+    
+    for ( int i = 0;  i < 4; i++ ){
+        
+        juce::String str;
+        str << (12 + i*12);
+        str << " dB/octv";
+        stringArray.add(str);
+        
+    }
+    
+    layout.add(
+               std::make_unique<juce::AudioParameterChoice>("LowCut Slope",
+                                                            "LowCut Slope",
+                                                            stringArray,
+                                                            0)
+               );
+    
+    layout.add(
+               std::make_unique<juce::AudioParameterChoice>("HighCut Slope",
+                                                            "HighCut Slope",
+                                                            stringArray,
+                                                            0)
+               );
+    
+    return layout;
+}
+
+
+
 
 //==============================================================================
 // This creates new instances of the plugin..
